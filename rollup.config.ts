1cca9a518854029b76fab4ab2c4b7fdb0b012370
Eva <17610603221@163.com>
2023-03-20 20:43:08 +0800

    new version

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,38 +1,20 @@
 import typescript from '@rollup/plugin-typescript'
 import terser from '@rollup/plugin-terser'
 import commonjs from '@rollup/plugin-commonjs'
-import json from '@rollup/plugin-json'
-import alias from '@rollup/plugin-alias'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import { defineConfig } from 'rollup'
-import pkg from './package.json'
-import path from 'path'
 
 const production = process.env.BUILD_MODE === 'production'
 
 export default defineConfig({
-  input: './lib/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      name: 'bundle',
-      format: 'umd',
-      globals: { lodash: 'lodash' },
-      sourcemap: !production
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      sourcemap: !production
-    }
-  ],
-  plugins: [
-    json(),
-    commonjs(),
-    typescript({ typescript: require('ttypescript') }),
-    nodeResolve({ browser: true }),
-    alias({ entries: { '@': path.resolve(__dirname, './lib') } })
-  ].concat(!production ? [] : [terser()]),
-  external: ['lodash'],
-  watch: { include: 'lib/**' }
+  input: './src/index.ts',
+  output: {
+    format: 'es',
+    file: 'dist/tlib-cli.js',
+    sourcemap: !production
+  },
+  plugins: [commonjs(), typescript(), nodeResolve({ browser: true })].concat(
+    !production ? [] : [terser()]
+  ),
+  watch: { include: 'src/**' }
 })
---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import prompts from 'prompts'
+import { fileURLToPath } from 'node:url'
+
+const cwd = process.cwd()
+const defaultTargetDir = 'typescript-lib'
+const renameFiles: Record<string, string> = {
+  _gitignore: '.gitignore',
+  _eslintrc: '.eslintrc.js',
+  _prettierrc: '.prettierrc.js',
+  _eslintignore: '.eslintignore'
+}
+
+async function init() {
+  let targetDir = defaultTargetDir
+  try {
+    await prompts([
+      {
+        type: 'text',
+        name: 'projectName',
+        message: 'Project name: ',
+        initial: defaultTargetDir,
+        onState: (state) => {
+          targetDir = state.value || defaultTargetDir
+        }
+      }
+    ])
+
+    const root = path.join(cwd, targetDir)
+
+    fs.mkdirSync(targetDir)
+
+    const templateDir = path.resolve(
+      fileURLToPath(import.meta.url),
+      '../..',
+      `template`
+    )
+
+    const write = (file: string, content?: string) => {
+      const targetPath = path.join(root, renameFiles[file] ?? file)
+      console.log({
+        templateDir,
+        targetPath,
+        templateFile: path.join(templateDir, file)
+      })
+      if (content) {
+        fs.writeFileSync(targetPath, content)
+      } else {
+        copy(path.join(templateDir, file), targetPath)
+      }
+    }
+
+    const files = fs.readdirSync(templateDir)
+    for (const file of files.filter((f) => f !== 'package.json')) {
+      write(file)
+    }
+
+    const pkg = JSON.parse(
+      fs.readFileSync(path.join(templateDir, 'package.json'), 'utf-8')
+    )
+
+    pkg.name = targetDir
+    write('package.json', JSON.stringify(pkg, null, 2) + '\n')
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function copy(src: string, dest: string) {
+  const stat = fs.statSync(src)
+  if (stat.isDirectory()) {
+    copyDir(src, dest)
+  } else {
+    fs.copyFileSync(src, dest)
+  }
+}
+
+function copyDir(srcDir: string, destDir: string) {
+  fs.mkdirSync(destDir, { recursive: true })
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file)
+    const destFile = path.resolve(destDir, file)
+    copy(srcFile, destFile)
+  }
+}
+
+init().catch((e) => {
+  console.log(e)
+})
